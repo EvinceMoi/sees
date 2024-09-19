be1a911fd88e22a52192bee56eb95c36ba84b93f
baneyue <baneyue@gmail.com>
2024-09-19 18:24:47 +0800

    add missing files

---
+++ third_party/mpvqt/README.md
@@ -0,0 +1,14 @@
+<!--
+SPDX-FileCopyrightText: 2023 George Florea Bănuș <georgefb899@gmail.com>
+
+SPDX-License-Identifier: CC0-1.0
+-->
+
+MpvQt is a [libmpv](https://github.com/mpv-player/mpv/) wrapper for Qt Quick 2/Qml, original from [MpvQt](https://invent.kde.org/libraries/mpvqt)
+
+## How to use
+
+- Create a class extending `MpvAbstractItem` (check the [mpvitem.h](examples/video-player/mpvitem.h)/[mpvitem.cpp](examples/video-player/mpvitem.cpp) files in the example)
+- Register the class `qmlRegisterType<ClassName>("com.example.mpv", 1, 0, "NameUsedInQml");`
+- In your qml file import mpv `import com.example.mpv 1.0`
+- Then create an instance `NameUsedInQml {}` (check the [Main.qml](examples/video-player/Main.qml) file in the example)
---
+++ third_party/mpvqt/examples/video-player/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * SPDX-FileCopyrightText: 2023 George Florea Bănuș <georgefb899@gmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickWindow>
+
+#include <MpvAbstractItem>
+#include "mpvitem.h"
+#include "mpvproperties.h"
+
+int main(int argc, char *argv[])
+{
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
+    QGuiApplication app(argc, argv);
+
+    qmlRegisterType<MpvItem>("com.example.mpv", 1, 0, "MpvItem");
+    qmlRegisterSingletonInstance("com.example.mpv", 1, 0, "MpvProperties", MpvProps::self());
+
+    QQmlApplicationEngine engine(&app);
+    const QUrl url(QStringLiteral("qrc:/Main.qml"));
+    auto onObjectCreated = [url](const QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl) {
+            QCoreApplication::exit(-1);
+        }
+    };
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, onObjectCreated, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
